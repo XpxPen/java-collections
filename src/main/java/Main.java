d012d5f2a6030515a2f8a13f92bef747ed697f42
Martin Burolla <mburolla@xpanxion.com>
2023-02-16 17:51:34 -0500

    update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,34 @@
+import Model.*;
+import Repo.DataRepo;
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("here");
+        ex1();
+    }
+
+    private static void ex1() { // Range of people
+        // TODO...
+
+    }
+
+    private static void ex2() { // Average Displacement
+        // TODO...
+
+    }
+
+    private static void ex3() { // People Uppercase
+        // TODO...
+
+    }
+
+    private static void ex4() { // Word counter
+        // TODO...
+
+    }
+
+    private static void ex5() { // Sorted word counter
+        // TODO...
+
     }
 }
