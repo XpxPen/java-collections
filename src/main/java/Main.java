c1a980ec971f8c9205a2a1620da3176c0e678ebd
Gilbert Cyr <gcyr@xpanxion.com>
2023-02-17 13:08:39 -0500

    Completed Exercise 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,32 @@
 import Model.*;
 import Repo.DataRepo;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
         ex1();
+        ex2();
     }
 
     private static void ex1() { // Range of people
-        // TODO...
-
+       var people = new ArrayList<Person>();
+       people = DataRepo.getPeople();
+       var filterList = people.stream()
+               .filter(p -> p.getAge() >= 20)
+               .filter(p -> p.getAge() <= 40)
+               .collect(Collectors.toList());
+       System.out.println(filterList);
     }
 
-    private static void ex2() { // Average Displacement
-        // TODO...
-
+    private static void ex2() { // Average Displacement\
+        var cars = new ArrayList<Car>();
+        cars = DataRepo.getCarsWithEngines();
+        var filterList = cars.stream()
+                .mapToDouble(c -> c.getEngine().getDisplacment())
+                .summaryStatistics();
+        //.average();
+        System.out.println(Math.round(filterList.getAverage() * 100.0) / 100.0);
     }
 
     private static void ex3() { // People Uppercase
