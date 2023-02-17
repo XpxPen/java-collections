c61729cab39a2252df94dcdda3466635ec4739df
Tue <tuedoan12@hotmail.com>
2023-02-17 15:44:39 -0500

    Ex# 3, 4, & 5

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,12 +2,13 @@ import Model.*;
 import Repo.DataRepo;
 
 import java.awt.*;
+import java.lang.reflect.Array;
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        ex2();
+        ex5();
     }
 
     private static void ex1() { // Range of people
@@ -36,16 +37,58 @@ public class Main {
 
     private static void ex3() { // People Uppercase
         // TODO...
+        ArrayList<Person> people = DataRepo.getPeople();
+        people.forEach(p -> {
+            p.setFirstName(p.getFirstName().toUpperCase());
+            p.setLastName(p.getLastName().toUpperCase());
+        });
 
+        System.out.println(people);
     }
 
     private static void ex4() { // Word counter
         // TODO...
+        HashMap<String, Integer> wordsCounter = new HashMap<String, Integer>();
+        ArrayList<String> words = DataRepo.readFile();
+
+        System.out.println(words);
+
+        words.forEach(w -> {
+            if (wordsCounter.containsKey(w)) {
+                wordsCounter.put(w, wordsCounter.get(w)+1);
+            } else {
+                wordsCounter.put(w, 1);
+            }
+        });
+
+        System.out.println(wordsCounter);
 
     }
 
     private static void ex5() { // Sorted word counter
         // TODO...
+        HashMap<String, Integer> wordsCounter = new HashMap<String, Integer>();
+        ArrayList<String> words = DataRepo.readFile();
+        ArrayList<Word> wordsList = new ArrayList<Word>();
 
+        System.out.println(words);
+
+        words.forEach(w -> {
+            if (wordsCounter.containsKey(w)) {
+                wordsCounter.put(w, wordsCounter.get(w)+1);
+            } else {
+                wordsCounter.put(w, 1);
+            }
+        });
+
+        System.out.println(wordsCounter);
+
+        wordsCounter.forEach((k, v) -> {
+            wordsList.add(new Word(k, v));
+        });
+
+        Collections.sort(wordsList);
+
+        System.out.println(wordsList);
     }
 }
