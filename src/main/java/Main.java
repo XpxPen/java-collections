d5e1b7b4acaac735a43394757826f80e68900022
Josue Gonzalez <josue.gonzalez@penske.com>
2023-02-17 17:09:37 -0500

    Have to fix ex3 and ex4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,34 +1,64 @@
 import Model.*;
 import Repo.DataRepo;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        ex1();
+        //ex1();
+        //ex2();
+        //ex3();
+        //ex4();
+        ex5();
     }
 
     private static void ex1() { // Range of people
-        // TODO...
+
+        var people = DataRepo.getPeople().stream()
+                .filter(p -> p.getAge() >= 20 && p.getAge() <=40)
+                .collect(Collectors.toList());
+
+        people.forEach(System.out::println);
 
     }
 
     private static void ex2() { // Average Displacement
-        // TODO...
+
+        var cars = ((float) DataRepo.getCarsWithEngines().stream()
+                .mapToDouble(c -> c.getEngine().getDisplacment())
+                .average()
+                .getAsDouble());
+
+        System.out.println(cars);
 
     }
 
     private static void ex3() { // People Uppercase
-        // TODO...
+        var people = DataRepo.getPeople().stream()
+                .map(p -> p.toString().toUpperCase())
+                .collect(Collectors.toList());
 
+        people.forEach(System.out::println);
     }
 
     private static void ex4() { // Word counter
-        // TODO...
+        var pairs = new HashMap<String, Long>();
+        var file = DataRepo.readFile().stream()
+                .map(f -> pairs.put(f , f.chars().count()))
+                .collect(Collectors.toList());
+
+        System.out.println(pairs);
 
     }
 
     private static void ex5() { // Sorted word counter
-        // TODO...
+        var pairs = new HashMap<String, Long>();
+        var file = DataRepo.readFile().stream()
+                .map(f -> pairs.put(f , f.chars().count()))
+                .collect(Collectors.toList());
+
+
+        System.out.println(pairs);
 
     }
 }
