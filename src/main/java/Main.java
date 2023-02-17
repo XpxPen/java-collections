f4277af0be3ee3c5126532983452afc27e46c431
Martin Burolla <mburolla@xpanxion.com>
2023-02-17 09:59:16 -0500

    update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        ex4();
+        ex5();
     }
 
     private static void ex1() { // Range of people
@@ -74,6 +74,29 @@ public class Main {
 
     private static void ex5() { // Sorted word counter
         // TODO...
+        var wordHashMap = new HashMap<String, Integer>();
 
+        var wordList = DataRepo.readFile();
+        for (String word : wordList) {
+            if (wordHashMap.containsKey(word)) {
+                // Increment the count for this word.
+                var count = wordHashMap.get(word);
+                wordHashMap.put(word, ++count);
+            } else {
+                // Add the word to the HashMap for the first time.
+                wordHashMap.put(word, 1);
+            }
+        }
+
+        var keySet = wordHashMap.keySet();
+        var retWordList = new ArrayList<Word>();
+        for (String key : keySet) {
+            retWordList.add(new Word(key, wordHashMap.get(key)));
+        }
+
+        Collections.sort(retWordList);
+        for (Word word : retWordList) {
+            System.out.println(word.getText() + ": " + word.getCount());
+        }
     }
 }
