713d6abd7edec8a5a7c73cd89e1030e670e3d145
Henok Berhe <Henok.Berhe@penske.com>
2023-02-17 13:53:00 -0500

    ex1 is working

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,14 +2,26 @@ import Model.*;
 import Repo.DataRepo;
 import java.util.*;
 
+
 public class Main {
     public static void main(String[] args) {
+       System.out.println("Hi henok");
         ex1();
     }
 
     private static void ex1() { // Range of people
-        // TODO...
 
+        var listOfPeople = DataRepo.getPeople();
+        // ArrayList<Person> listOfPeople = DataRepo.getPeople();
+          ArrayList<Person> selectedPeople = new ArrayList<Person>();
+          for(Person person : listOfPeople){
+              if(person.getAge() >=20 && person.getAge() <=40){
+                  selectedPeople.add(person);
+              }
+
+          }
+
+        System.out.println(selectedPeople);
     }
 
     private static void ex2() { // Average Displacement
