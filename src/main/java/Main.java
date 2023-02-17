0d85b7352bccbfef55d18dc60a671327fc43b947
meganlukes <meglukes@gmail.com>
2023-02-17 14:38:05 -0500

    Completed first two exercises

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,30 +5,48 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         ex1();
+        ex2();
+        ex3();
+        ex4();
+        ex5();
     }
 
     private static void ex1() { // Range of people
-        // TODO...
 
+        var peopleList = DataRepo.getPeople();
+        var shortList = new ArrayList<Person>();
+        for (Person p : peopleList){
+            if (p.getAge() >= 20 && p.getAge() <= 40){
+                shortList.add(p);
+            }
+        }
+        //System.out.println(shortList);
     }
 
     private static void ex2() { // Average Displacement
-        // TODO...
 
+        var carEngineList = DataRepo.getCarsWithEngines();
+        float sum = 0f;
+        float tot = 0f;
+        for (var car : carEngineList){
+            sum += car.getEngine().getDisplacment();
+            tot++;
+        }
+        float avg = sum/tot;
+        System.out.println("The average displacement is: " + avg);
     }
 
     private static void ex3() { // People Uppercase
-        // TODO...
-
+        System.out.println("Testing");
     }
 
     private static void ex4() { // Word counter
-        // TODO...
+
 
     }
 
     private static void ex5() { // Sorted word counter
-        // TODO...
+
 
     }
 }
