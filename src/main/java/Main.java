f9ff0329e6201c5b6b12c181e2b1be97a115cf07
Carson Winchester <carson.winchester@penske.com>
2023-02-17 21:56:32 -0500

    Ex2: Average Displacement

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,25 +1,29 @@
 import Model.*;
 import Repo.DataRepo;
 import java.util.*;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        ex1();
+        ex2();
     }
 
     private static void ex1() { // Range of people
        ArrayList<Person> people = DataRepo.getPeople();
        for (Person person: people) {
-           if(person.getAge() >= 20 && person.getAge() <= 40) {
+           if (person.getAge() >= 20 && person.getAge() <= 40) {
                System.out.print(person);
            }
        }
     }
 
     private static void ex2() { // Average Displacement
-        // TODO...
-
-    }
+       ArrayList<Car> cars = DataRepo.getCarsWithEngines();
+        OptionalDouble engines = cars.stream()
+                .mapToDouble(d -> d.getEngine().getDisplacment())
+                .average();
+        System.out.print(engines.getAsDouble());
+       }
 
     private static void ex3() { // People Uppercase
         // TODO...
