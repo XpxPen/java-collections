4d666acac009301703a0a703d082557aa5479501
Carson Winchester <carson.winchester@penske.com>
2023-02-20 11:18:14 -0500

    Ex3 & Ex4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,22 @@
 import Model.*;
 import Repo.DataRepo;
 import java.util.*;
+import java.util.stream.Collectors;
+import static java.util.stream.Collectors.toList;
 
 public class Main {
     public static void main(String[] args) {
+        ex1();
         ex2();
+        ex3();
+        ex4();
     }
 
     private static void ex1() { // Range of people
        ArrayList<Person> people = DataRepo.getPeople();
-       for (Person person: people) {
-           if (person.getAge() >= 20 && person.getAge() <= 40) {
-               System.out.print(person);
+       for (Person p: people) {
+           if (p.getAge() >= 20 && p.getAge() <= 40) {
+               System.out.print(p);
            }
        }
     }
@@ -24,17 +29,22 @@ public class Main {
        }
 
     private static void ex3() { // People Uppercase
-        // TODO...
-
+        var people = DataRepo.getPeople().stream()
+                .map(person -> person.toString().toUpperCase())
+                .collect(toList());
+        people.forEach(System.out::print);
     }
 
     private static void ex4() { // Word counter
-        // TODO...
+        var words = new HashMap<String, Long>();
+        var file = DataRepo.readFile().stream()
+                .map(w -> words.put(w , w.chars().count()))
+                .collect(Collectors.toList());
 
+        System.out.println(words);
     }
 
     private static void ex5() { // Sorted word counter
-        // TODO...
 
     }
 }
