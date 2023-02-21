b0b92ae446f6d0c3e2f108eddf7cf1c4254596e6
Kevin Perry <kevin.perry@penske.com>
2023-02-21 15:59:20 -0500

    Ex2 and Ex3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,12 @@
 import Model.*;
 import Repo.DataRepo;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
+
     public static void main(String[] args) {
-        ex1();
+        ex1(); ex2(); ex3();
     }
 
     private static void ex1() { // Range of people
@@ -18,15 +20,24 @@ public class Main {
     }
 
     private static void ex2() { // Average Displacement
+        float average = 0;
         var cars = DataRepo.getCarsWithEngines();
-        var displacementAverage = new ArrayList<>();
-
-
+        var displacementAverage = cars.stream()
+                .map(Car::getEngine)
+                .collect(Collectors.toList());
+        for (Engine e : displacementAverage) {
+            average += e.getDisplacment();
+        }
+        System.out.println(average / displacementAverage.size());
     }
 
     private static void ex3() { // People Uppercase
-        // TODO...
-
+        var peopleUpper = DataRepo.getPeople();
+        peopleUpper.stream().forEach(
+            person -> person.setFirstName(person.getFirstName().toUpperCase()));
+        peopleUpper.stream().forEach(
+            person -> person.setLastName(person.getLastName().toUpperCase()));
+        System.out.println(peopleUpper);
     }
 
     private static void ex4() { // Word counter
